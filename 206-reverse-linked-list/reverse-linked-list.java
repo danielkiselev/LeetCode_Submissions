175b977533e52267153b667c198a8a6c2d622a27
Daniel Kiselev <dannykiselev@gmail.com>
2024-12-01 18:15:38 -0500

    Time: 1 ms (100.00%) | Memory: 43 MB (7.37%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,30 +8,58 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     ListNode holder;
+
+//     public ListNode reverseList(ListNode head) {
+//         if (head == null || head.next == null) {
+
+//             return head;
+//         }
+//         trav(head).next = null;
+//         return holder;
+
+//     }
+
+//     ListNode trav(ListNode n) {
+//         if (n.next != null) {
+//             ListNode res;
+//             res = trav(n.next);
+//             res.next = n;
+//             return n;
+
+//         } else {
+//             holder = n;
+//             return n;
+//         }
+
+//     }
+// }
+
+
 class Solution {
-    ListNode holder;
 
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
-
             return head;
         }
-        trav(head).next = null;
-        return holder;
-
-    }
-
-    ListNode trav(ListNode n) {
-        if (n.next != null) {
-            ListNode res;
-            res = trav(n.next);
-            res.next = n;
-            return n;
-
-        } else {
-            holder = n;
-            return n;
+        ArrayDeque<ListNode> holder = new ArrayDeque<ListNode>();
+        ListNode curr = head;
+        while(curr.next!=null){
+            ListNode temp = curr.next;
+            curr.next = null;
+            holder.push(curr);
+            curr = temp;
         }
-
+        head = curr;
+        while(!holder.isEmpty()){
+            ListNode temp = holder.pop();
+            curr.next = temp;
+            curr = temp;
+        }
+        return head;
+            
+        
+    
     }
 }
