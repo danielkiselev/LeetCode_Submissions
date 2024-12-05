ca1319d4fbb4cd2a02cf6b706f6240879c8f5eb4
Daniel Kiselev <dannykiselev@gmail.com>
2024-12-04 20:22:14 -0500

    Time: 97 ms (72.59%) | Memory: 101.2 MB (45.94%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -1,40 +1,70 @@
 class Bank {
+    long[] accountBalance;
 
-    long[] balance;
-    int n;
 
     public Bank(long[] balance) {
-        this.balance = balance;
-        n = balance.length;
+        accountBalance = balance;
+        
     }
-
+    
     public boolean transfer(int account1, int account2, long money) {
-        account1 -= 1;
-        account2 -= 1;
-        if ((account1 < n && account2 < n) && (balance[account1] >= money)) {
-            balance[account1] -= money;
-            balance[account2] += money;
+        if(!checkAccount(account2)){
+            return false;
+        }
+        if(checkAccountFunds(account1, money)){
+            accountBalance[account1-1] -= money;
+            accountBalance[account2-1] += money;
             return true;
         }
         return false;
-
+        
+        
     }
-
+    
     public boolean deposit(int account, long money) {
-        account -= 1;
-        if (account < n) {
-            balance[account] += money;
+        if(checkAccount(account)){
+            accountBalance[account-1] += money;
+            return true;
+        }
+        return false;
+        
+    }
+    
+    public boolean withdraw(int account, long money) {
+         if(checkAccountFunds(account, money)){
+            accountBalance[account-1] -= money;
+            return true;
+        }
+        return false;
+        
+    }
+
+    private boolean checkAccount(int acc){
+        if(accountBalance.length<acc){
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean checkAccountFunds(int acc, long money){
+        if(!checkAccount(acc)){
+            return false;
+        }
+        long currBalance = accountBalance[acc-1];
+        if(currBalance>=money){
             return true;
         }
         return false;
     }
 
-    public boolean withdraw(int account, long money) {
-        account -= 1;
-        if (account < n && (balance[account] >= money)) {
-            balance[account] -= money;
-            return true;
-        }
-        return false;
-    }
+
 }
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
