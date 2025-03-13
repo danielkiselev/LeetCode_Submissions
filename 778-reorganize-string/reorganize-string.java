25ec9d9ec1f96626d37b576d0591f96749c772d0
Daniel Kiselev <dannykiselev@gmail.com>
2025-03-13 16:50:29 -0400

    Time: 213 ms (5.56%) | Memory: 45.5 MB (5.11%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int [] letterHolder = new int[26];
+        for(char c: s.toCharArray()){
+            int currI = c-'0'-49;
+            letterHolder[currI]++;
+            System.out.println(currI);
+        }
+        StringBuilder sb = new StringBuilder();
+        int prev = -1;
+        while(sb.length()<s.length()){
+            int maxI = -1;
+            for(int i=0; i<26; i++){
+                if(prev != i && letterHolder[i]>0){
+                    System.out.println(+i+" ~ "+letterHolder[i]);
+                    if(maxI == -1){
+                        maxI = i;
+                    }
+                    
+                    else if(letterHolder[i] > letterHolder[maxI]){
+                        System.out.println(letterHolder[i]);
+                        System.out.println(letterHolder[maxI]);
+                        maxI = i;
+                    }
+                }    
+            }
+            if(maxI == -1){
+                return "";
+            }
+            sb.append(Character.toString(maxI+'0'+49));
+            letterHolder[maxI]--;
+            prev = maxI;
+
+
+
+        }
+        return sb.toString();
+        
+    }
+}
