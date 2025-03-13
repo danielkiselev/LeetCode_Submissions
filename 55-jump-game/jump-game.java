6fbe6ec609a084aebebb7e63b6037c7cb53554b8
Daniel Kiselev <dannykiselev@gmail.com>
2025-03-13 10:51:48 -0400

    Time: 2 ms (79.84%) | Memory: 45.8 MB (54.66%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,22 +1,19 @@
 class Solution {
 
     public boolean canJump(int[] nums) {
-        int[] visited = new int[nums.length];
-        int currIndex = nums.length-1;
-            for(int i=currIndex-1;i>=0; i--){
-                int checkIndex = i;
-                int checkNum = nums[i];
-                if(currIndex<=checkNum+checkIndex){
-                    currIndex = checkIndex;
-                }
+        int currIndex = nums.length - 1;
+        for (int i = currIndex - 1; i >= 0; i--) {
+            int checkIndex = i;
+            int checkNum = nums[i];
+            if (currIndex <= checkNum + checkIndex) {
+                currIndex = checkIndex;
             }
-        if(currIndex == 0){
+        }
+        if (currIndex == 0) {
             return true;
         }
 
-        
+        return false;
 
-        return false; 
-        
     }
 }
