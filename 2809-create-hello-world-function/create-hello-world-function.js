155ec67efa340c97d51a8c1a0452983cebe67abb
Daniel Kiselev <dannykiselev@gmail.com>
2025-03-13 10:23:55 -0400

    Time: 30 ms (98.67%) | Memory: 53 MB (30.59%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -2,11 +2,8 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
     return function(...args) {
-        const helloWorldString = "Hello World";
-        return helloWorldString;
-        
+        return "Hello World";
     }
 };
 
