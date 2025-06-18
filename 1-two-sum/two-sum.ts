e8024565a96ec8669623f4cca3903195712d5d7b
Daniel Kiselev <dannykiselev@gmail.com>
2025-06-18 18:41:10 -0400

    Time: 1 ms (88.87%) | Memory: 56.6 MB (58.93%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,15 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-    let res: number[];
-    let visited= {}; // value required, index
+    const visited= {}; // value required, index
     for (let i = 0; i < nums.length; i++) {
         const curr = nums[i];
         if(visited[curr] !== undefined){
-            res=[visited[curr], i];
-            return res;
+            return [visited[curr], i];
         }
         else{
             visited[target-curr]=i;
         }
     }
-    return res;
+    return [];
 };
