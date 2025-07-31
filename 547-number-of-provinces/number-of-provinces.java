8faf0d40cbfac0076928de6d75a0d5b98806ff5a
Daniel Kiselev <dannykiselev@gmail.com>
2025-07-30 20:38:14 -0400

    Time: 1 ms (87.13%) | Memory: 47.8 MB (62.37%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,36 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int[] visited = new int[n];
-        ArrayDeque <Integer> procQueue = new ArrayDeque <Integer>();
-        procQueue.add(0);
-        int counter = 0;
-        int index = 1; 
-        while(!procQueue.isEmpty()){
-            int visiting = procQueue.poll();
-            visited[visiting] = 1;
-            for(int j = 0; j<n; j++){
-                if(visited[j] == 1){
-                    continue;
-                }
-                else if(isConnected[visiting][j] == 1){
-                    procQueue.add(j);
-                    visited[j] = 1;
-                }
-            }
-            if(procQueue.isEmpty()){
-                counter++;
-                for(int i = index; i<n; i++){
-                    if(visited[i] == 0){
-                        procQueue.add(i);
-                        visited[i] = 1;
-                        index = i;
-                        break;
-                    }
-                }
+        int[] visited = new int[isConnected.length];
+        int count = 0;
+        for(int i =0 ;i < isConnected.length ; i++){
+            if(visited[i] == 0){
+                count ++;
+                dfs(i,isConnected,visited);
             }
         }
-        return counter;
-        
+        return count;
+    }
+
+    private void dfs(int node, int[][] connected, int[] visited){
+        visited[node] = 1;
+        for(int j = 0; j < connected.length ; j ++){
+            if(connected[node][j] == 1 && visited[j] == 0){
+                visited[j] = 1;
+                dfs(j,connected, visited);
+            }
+        }
     }
 }
