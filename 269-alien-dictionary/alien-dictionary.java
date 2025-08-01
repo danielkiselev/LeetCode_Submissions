dae4bfeb7cd6e2c4bdfd89625096bdb44159b74b
Daniel Kiselev <dannykiselev@gmail.com>
2025-08-01 15:39:56 -0400

    Time: 4 ms (83.74%) | Memory: 42 MB (74.57%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,24 +1,11 @@
 class Solution {
-    private class Node {
-        String letter;
-        ArrayList<String> children = new ArrayList<String>();
-        public Node(String letter) {
-            this.letter = letter;
-        }
-        public void addChild(String s) {
-            children.add(s);
-        }
-    }
     HashSet<String> visited = new HashSet<String>();
     HashMap<String, Node> graph = new HashMap<String, Node>();
     public String alienOrder(String[] words) {
-        for (int i = 0; i < words.length; i++) {
-            String curr = words[i];
+        for (String curr: words) {
             for (int z = 0; z < curr.length(); z++) {
                 String letter = curr.substring(z, z + 1);
-                if (graph.containsKey(letter)) {
-                    continue;
-                } else {
+                if (!graph.containsKey(letter)) {
                     Node n = new Node(letter);
                     graph.put(letter, n);
                 }
@@ -27,7 +14,7 @@ class Solution {
         for (int i = 0; i < (words.length - 1); i++) {
             String word1 = words[i];
             String word2 = words[i + 1];
-            if (word1.length() > word2.length() && word1.substring(0, word2.length()).equals(word2)) {
+            if (word1.length() > word2.length() && word1.startsWith(word2)) {
                 return "";
             }
             for (int z = 0; z < word1.length(); z++) {
@@ -35,9 +22,7 @@ class Solution {
                 if (z < word2Sz) {
                     String l1 = word1.substring(z, z + 1);
                     String l2 = word2.substring(z, z + 1);
-                    if (l1.equals(l2)) {
-                        continue;
-                    } else {
+                    if (!l1.equals(l2)) {
                         Node n = graph.get(l1);
                         n.addChild(l2);
                         Node test = graph.get(l2);
@@ -53,17 +38,15 @@ class Solution {
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (String key : graph.keySet()) {
-            if (visited.contains(key)) {
-                continue;
-            } else {
+        for (String key: graph.keySet()) {
+            if (!visited.contains(key)) {
                 Node n = graph.get(key);
                 if (n.children.isEmpty()) {
                     visited.add(n.letter);
                     sb.append(n.letter);
                 } else {
                     String res = sort(n, 1);
-                    if (res.equals("")) {
+                    if (res.isEmpty()) {
                         return "";
                     }
                     sb.append(res);
@@ -83,12 +66,11 @@ class Solution {
             visited.add(n.letter);
             return sb.toString();
         }
-        for (String child : n.children) {
-            if (visited.contains(child)) {
-            } else {
+        for (String child: n.children) {
+            if (!visited.contains(child)) {
                 Node childN = graph.get(child);
                 String res = sort(childN, depth + 1);
-                if (res.equals("")) {
+                if (res.isEmpty()) {
                     return "";
                 }
                 sb.append(res);
@@ -97,5 +79,15 @@ class Solution {
         visited.add(n.letter);
         sb.append(n.letter);
         return sb.toString();
+    }
+    private class Node {
+        String letter;
+        ArrayList<String> children = new ArrayList<String>();
+        public Node(String letter) {
+            this.letter = letter;
+        }
+        public void addChild(String s) {
+            children.add(s);
+        }
     }
 }
