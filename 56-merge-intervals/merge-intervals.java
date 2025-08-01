16d37048f4855bc69d6e9acb4a52238738816626
Daniel Kiselev <dannykiselev@gmail.com>
2025-07-31 20:39:19 -0400

    Time: 5 ms (99.10%) | Memory: 46.7 MB (39.37%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -12,7 +12,7 @@ class Solution {
         Arrays.sort(end);
         int sCounter = 0;
         int eCounter = 0;
-        ArrayList <Integer []> res = new ArrayList <Integer []>();
+        ArrayList <int []> res = new ArrayList <>();
         while(sCounter<start.length && eCounter<end.length ){
             int currStart = start[sCounter]; 
             int currEnd = end[eCounter]; 
@@ -20,7 +20,7 @@ class Solution {
                 eCounter++;
             }
             else{
-                Integer[] temp = new Integer[2];
+                int[] temp = new int[2];
                 temp[0] =currStart;
                 temp[1] =currEnd;
                 res.add(temp);
@@ -29,42 +29,9 @@ class Solution {
             }
         }
         int[][] primitiveArray = new int[res.size()][2];
-
-        // Iterate through the ArrayList and copy elements to the int array
         for (int i = 0; i < res.size(); i++) {
-            primitiveArray[i][0] = res.get(i)[0];
-            primitiveArray[i][1] = res.get(i)[1];
+            primitiveArray[i] = res.get(i);
         }
         return primitiveArray;
     }
 }
-
-
-        // int start [] = new int[intervals.size()]; 
-        // int end [] = new int[intervals.size()]; 
-        // int index = 0;
-        // for(Interval i : intervals){
-        //     start[index] = i.start;
-        //     end[index] = i.end;
-        //     index++;
-        // }
-        // Arrays.sort(start);
-        // Arrays.sort(end);
-        // int sCounter = 0;
-        // int eCounter = 0;
-
-        // ArrayList <Interval> res = new ArrayList <Interval>();
-        // while(sCounter<start.length && eCounter<end.length ){
-        //     int currStart = start[sCounter]; 
-        //     int currEnd = end[eCounter]; 
-        //     if((eCounter+1)<end.length && currEnd>=start[(eCounter+1)]){
-        //         eCounter++;
-        //     }
-        //     else{
-        //         Interval temp = new Interval(currStart, currEnd);
-        //         res.add(temp);
-        //         sCounter = eCounter+1;
-        //         eCounter++;
-        //     }
-        // }
-        // return res;
