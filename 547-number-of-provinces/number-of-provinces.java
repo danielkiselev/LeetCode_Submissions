e41674553df57886c23772ae098fab9274fc546d
Daniel Kiselev <dannykiselev@gmail.com>
2025-08-07 08:59:58 -0400

    Time: 2 ms (46.27%) | Memory: 47.8 MB (55.59%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,26 +1,25 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         boolean[] visited = new boolean[isConnected.length];
-        ArrayDeque <Integer> procQueue = new ArrayDeque <Integer>();
+        ArrayDeque<Integer> procQueue = new ArrayDeque<Integer>();
         procQueue.add(0);
         int counter = 0;
-        int index = 1; 
-        while(!procQueue.isEmpty()){
+        int index = 1;
+        while (!procQueue.isEmpty()) {
             int visiting = procQueue.poll();
             visited[visiting] = true;
-            for(int j = 0; j<visited.length; j++){
-                if(visited[j]){
+            for (int j = 0; j < visited.length; j++) {
+                if (visited[j]) {
                     continue;
-                }
-                else if(isConnected[visiting][j] == 1){
+                } else if (isConnected[visiting][j] == 1) {
                     procQueue.add(j);
                     visited[j] = true;
                 }
             }
-            if(procQueue.isEmpty()){
+            if (procQueue.isEmpty()) {
                 counter++;
-                for(int i = index; i<visited.length; i++){
-                    if(!visited[i]){
+                for (int i = index; i < visited.length; i++) {
+                    if (!visited[i]) {
                         procQueue.add(i);
                         visited[i] = true;
                         index = i;
@@ -30,6 +29,6 @@ class Solution {
             }
         }
         return counter;
-        
+
     }
 }
